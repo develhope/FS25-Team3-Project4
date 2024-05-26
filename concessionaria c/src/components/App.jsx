8c1250ac4a6e4e88b3f3be0596ca3a4689a362a3
Andros924 <sir_vivi@hotmail.it>
2024-05-26 23:38:48 +0200

    ho cancellato le mie modifiche senza toccare il resto del codice

--- concessionaria c/src/components/App.jsx
+++ concessionaria c/src/components/App.jsx
@@ -7,29 +7,25 @@ import '../styles/App.css';
 import { RegistrationProvider } from '../contexts/RegistrationContext';
 import { SelectedBikesProvider } from '../contexts/SelectedBikesContext';
 import '../styles/backgroundimage.css';
-import Amministratore from './Amministratore';
-import { AdminContext, AdminProvider } from '../contexts/AdminContext';
+
 
 
 function App() {
   return (
     <Router>
-      <AdminProvider>
-        <RegistrationProvider>
-          <SelectedBikesProvider>
-            <div className="app-container">
-              <div className='background-image'></div>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/gamma" element={<Gamma />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path='/Ammistratore' element={<Amministratore />} />
-              </Routes>
-            </div>
-          </SelectedBikesProvider>
-        </RegistrationProvider>
-      </AdminProvider>
+      <RegistrationProvider>
+        <SelectedBikesProvider>
+          <div className="app-container">
+            <div className='background-image'></div>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/gamma" element={<Gamma />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </div>
+        </SelectedBikesProvider>
+      </RegistrationProvider>
     </Router>
   );
 }
